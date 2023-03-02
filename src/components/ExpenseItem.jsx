0a8a26cf89c066d6b3e166837f62000579ec667b
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-03-02 11:46:04 -0500

    props

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,16 +1,16 @@
 import "./ExpenseItem.scss";
 
-function ExpenseItem() {
-	const expenseDate = new Date(2021, 2, 28).toDateString();
-	const expenseTitle = "Car Insurance";
-	const expenseAmount = 249.67;
+function ExpenseItem(props) {
+	// const expenseDate = new Date(2021, 2, 28).toDateString();
+	// const expenseTitle = "Car Insurance";
+	// const expenseAmount = 249.67;
 
 	return (
 		<div className="expense-item">
-			<div>{expenseDate}</div>
+			<div>{props.date.toDateString()}</div>
 			<div className="expense-item__description">
-				<h2>{expenseTitle}</h2>
-				<div className="expense-item__price">${expenseAmount}</div>
+				<h2>{props.title}</h2>
+				<div className="expense-item__price">${props.amount}</div>
 			</div>
 		</div>
 	);
