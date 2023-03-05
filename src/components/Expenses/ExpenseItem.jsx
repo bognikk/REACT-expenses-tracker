b8f6cb5d391ada87261e9ce97534896e45f69bbb
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-03-05 18:55:07 -0500

    rendering lists

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -3,12 +3,22 @@ import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.scss";
 
 const ExpenseItem = (props) => {
+	let priceClasses;
+
+	if (props.amount >= 100 && props.amount < 500) {
+		priceClasses = "expense-item__price medium";
+	} else if (props.amount >= 500) {
+		priceClasses = "expense-item__price high";
+	} else {
+		priceClasses = "expense-item__price";
+	}
+
 	return (
 		<Card className="expense-item">
 			<div className="expense-item__description">
 				<ExpenseDate date={props.date} />
 				<h2 className="expense-item__title">{props.title}</h2>
-				<div className="expense-item__price">${props.amount}</div>
+				<div className={priceClasses}>${props.amount}</div>
 			</div>
 			{/* <button>Remove</button> */}
 		</Card>
--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -13,16 +13,6 @@ const Expenses = (props) => {
 		setFilteredYear(selectedYear);
 	};
 
-	// const priceclassesHandler = (props) => {
-	// 	if (props.items[0].amount >= 100) {
-	// 		setPriceClasses("expense-item__price medium");
-	// 	} else if (props.items[0].amount >= 500) {
-	// 		setPriceClasses("expense-item__price high");
-	// 	} else {
-	// 		setPriceClasses("expense-item__price low");
-	// 	}
-	// };
-
 	return (
 		<>
 			<Card className="expenses">
@@ -30,26 +20,13 @@ const Expenses = (props) => {
 					onChangeFilter={filterChangeHandler}
 					selected={filteredYear}
 				/>
-				<ExpenseItem
-					title={props.items[0].title}
-					amount={props.items[0].amount}
-					date={props.items[0].date}
-				/>
-				<ExpenseItem
-					title={props.items[1].title}
-					amount={props.items[1].amount}
-					date={props.items[1].date}
-				/>
-				<ExpenseItem
-					title={props.items[2].title}
-					amount={props.items[2].amount}
-					date={props.items[2].date}
-				/>
-				<ExpenseItem
-					title={props.items[3].title}
-					amount={props.items[3].amount}
-					date={props.items[3].date}
-				/>
+				{props.items.map((expense) => (
+					<ExpenseItem
+						title={expense.title}
+						amount={expense.amount}
+						date={expense.date}
+					/>
+				))}
 			</Card>
 		</>
 	);
