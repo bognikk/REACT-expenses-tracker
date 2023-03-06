2d31aac3059eed4559f8e1112e8d246213b29dc7
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-03-06 11:19:42 -0500

    showing form conditionally

--- src/components/NewExpense/NewExpense.jsx
+++ src/components/NewExpense/NewExpense.jsx
@@ -2,19 +2,39 @@ import ExpenseForm from "./ExpenseForm";
 import Card from "../UI/Card";
 
 import "./NewExpense.scss";
+import { useState } from "react";
 
 const NewExpense = (props) => {
+	const [isEditing, setIsEditing] = useState(false);
+
 	const saveExpanseDataHandler = (enteredExpenseData) => {
 		const expenseData = {
 			...enteredExpenseData,
 			id: Math.random().toString(),
 		};
 		props.onAddExpense(expenseData);
+		setIsEditing(false);
+	};
+
+	const startEditingHandler = () => {
+		setIsEditing(true);
+	};
+
+	const stopEditingHandler = () => {
+		setIsEditing(false);
 	};
 
 	return (
 		<Card className="new-expense">
-			<ExpenseForm onSaveExpanseData={saveExpanseDataHandler} />
+			{!isEditing && (
+				<button onClick={startEditingHandler}>Add New Expense</button>
+			)}
+			{isEditing && (
+				<ExpenseForm
+					onSaveExpanseData={saveExpanseDataHandler}
+					onCancel={stopEditingHandler}
+				/>
+			)}
 		</Card>
 	);
 };
